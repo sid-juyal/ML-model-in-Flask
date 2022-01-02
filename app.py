8a3ec46933f71ad915ed456eea1aa680f75d3bf2
sid-juyal <siddharth.amity9b@gmail.com>
2022-01-03 01:54:27 +0530

    flask

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import joblib
+#import sys
+#sys.modules['sklearn.externals.joblib'] = joblib
+
+app = Flask(__name__)
+
+
+model = joblib.load("model.pkl")
+
+@app.route('/')
+def home():
+	return render_template("index.html")
+
+@app.route('/', methods=['POST'])
+def main():
+	if request.method == 'POST':
+		num = float(request.form['number'])
+		marks = model.predict([[num]])
+	return render_template("index.html", marks = marks)
+
+
+if __name__ == '__main__':
+	app.run(debug = True)
